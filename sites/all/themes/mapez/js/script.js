dd5d51f83d79d1f3a5f6f54681fa1569b280f318
Alexei Gorobets <asgorobets@gmail.com>
2013-06-10 01:20:14 +0300

    Add panning functionality. Add logo.

--- sites/all/themes/mapez/js/script.js
+++ sites/all/themes/mapez/js/script.js
@@ -1,3 +1,25 @@
-(function($) {
-  $(document).ready(function() {$('#leaflet-map').height($(document).height()-75)});
+(function ($) {
+
+  Drupal.behaviors.mapezMap = {
+    attach: function (context, settings) {
+      $('#leaflet-map-wrapper #leaflet-map').height($(window).height() -70);
+
+      $('.results-list li').click(function() {
+        var lat = $(this).children('.result-lat').html();
+        var lon = $(this).children('.result-lon').html();
+        Drupal.settings.leaflet[0].lMap.panTo([lat, lon]);
+        var layers = Drupal.settings.leaflet[0].lMap._layers;
+        for (var prop in layers) {
+          if (!isNaN(prop)) {
+            if (lat == Drupal.settings.leaflet[0].lMap._layers[prop]._latlng.lat
+              && lon == Drupal.settings.leaflet[0].lMap._layers[prop]._latlng.lng) {
+              Drupal.settings.leaflet[0].lMap._layers[prop].openPopup();
+            }
+          }
+        }
+      });
+    }
+  };
+
+
 })(jQuery);
